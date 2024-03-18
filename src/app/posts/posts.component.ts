a08be887b65ff4b24e5eb71b0cc3666fccbfd47d
andrewpersaud1 <andrewpersaud022@gmail.com>
2024-03-18 12:15:04 -0400

    update event

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { subscribeOn } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
@@ -7,7 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './posts.component.scss'
 })
 export class PostsComponent {
-  constructor(httpClient: HttpClient){
+  posts: any[] = [];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
+  constructor(private httpClient: HttpClient){
+    this.httpClient.get<any[]>(this.url)
+      .subscribe(response => {
+          this.posts = response;
+      });
   }
+    createPost(input: HTMLInputElement){
+        let post: {title: string, id?:number} = {title: input.value};
+        input.value= '';
+
+
+        this.httpClient.post<any>(this.url,JSON.stringify(post))
+        .subscribe(response =>{
+          post.id = response.id;
+          this.posts.unshift(post);
+        });
+    }
+    updatePost(post: {id: number, isRead?:boolean}){
+      this.httpClient.patch(this.url + '/'+ post.id, JSON.stringify({isRead:true}))
+        .subscribe(response=>{
+          console.log(response);
+        })
+    }
+
 }
